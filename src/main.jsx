f47c5e2772a140fb8853f20fcd01d2431a99f6a9
PanseeBadry <panseemohamed310@gmail.com>
2025-04-09 15:02:26 +0200

    update deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, HashRouter } from "react-router";
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 )
